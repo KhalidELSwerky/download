3bc6aba6bfc0e71aa61486aa0c17f0fef4b04698
KhalidELSwerky <Kjmal3839@gmail.com>
2025-03-18 01:53:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let select =document.getElementById("select1");
+let select    =  document.getElementById("select1");
 let downmanag =document.getElementById("manager-app");
+let windows11 =document.getElementById("windows-version");
+let windows12 =document.getElementById("windows-app");
+let app =document.getElementById("app");
 
 console.log(select.value)
 
@@ -7,8 +10,30 @@ console.log(select.value)
     if(select.value=="download-manager"){
        
         downmanag.style.display= "block";
+        windows11.style.display= "none";
+        windows12.style.display= "none";
     }
     else if(select.value==""){
+        windows11.style.display= "none";
+        downmanag.style.display= "none";
+        windows12.style.display= "none";
+
+    }
+
+    
+    else if(select.value=="windows1"){
+        windows12.style.display= "none";
+        windows11.style.display= "block";
         downmanag.style.display= "none";
     }
+    
+    else if(select.value=="windows2"){
+        
+        windows11.style.display= "none";
+        windows12.style.display= "block";
+        downmanag.style.display= "none";
+    }
+    
+
+
 }
